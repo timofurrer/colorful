fa963641c07542a73e5df4ad5b6274c85c7054f2
Timo Furrer <tuxtimo@gmail.com>
2017-04-09 16:47:35 +0200

    Add tests for sanitze color palette function

---
+++ tests/test_rgb.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+    colorful
+    ~~~~~~~~
+
+    Terminal string styling done right, in Python.
+
+    :copyright: (c) 2017 by Timo Furrer <tuxtimo@gmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+
+import os
+
+import pytest
+
+# do not overwrite module
+os.environ['COLORFUL_NO_MODULE_OVERWRITE'] = '1'
+
+import colorful.rgb as rgb  # noqa
+
+
+@pytest.mark.parametrize('colorpalette, expected', [
+    ({'black': '#000000'}, {'black': (0, 0, 0)}),
+    ({'red': '#FF0000'}, {'red': (255, 0, 0)}),
+    ({'purple': '#800080'}, {'purple': (128, 0, 128)}),
+    ({
+        'maroon': '#800000',
+        'magenta': '#FF00FF',
+        'cyan': '#00FFFF'
+    }, {
+        'maroon': (128, 0, 0),
+        'magenta': (255, 0, 255),
+        'cyan': (0, 255, 255)
+    })
+])
+def test_sanitize_color_palette(colorpalette, expected):
+    """
+    Test sanitizing a color palette
+    """
+    assert rgb.sanitize_color_palette(colorpalette) == expected
