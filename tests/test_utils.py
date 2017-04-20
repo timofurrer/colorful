296a85de915b5d61f31084d5202679e5210650c3
Timo Furrer <tuxtimo@gmail.com>
2017-04-20 15:57:18 +0200

    Improve RGB hex validate function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -29,20 +29,27 @@ import colorful.utils as utils  # noqa
     '#FFFF00',
     '#00FFFF',
     '#EF8BA0',
+    '#012345',
+    '#999FFF',
+    '#05fecA'
 ])
 def test_hex_to_rgb_conversion(hex_value):
     """
     Test the conversion from a RGB hex value to a RGB channel triplet
     """
     red, green, blue = utils.hex_to_rgb(hex_value)
-    assert '#{:02X}{:02X}{:02X}'.format(red, green, blue) == hex_value
+    assert '#{:02X}{:02X}{:02X}'.format(red, green, blue).lower() == hex_value.lower()
 
 
 @pytest.mark.parametrize('hex_error_value', [
     '#FFFFF',
     '#FFFFFFF',
     '#FFFFFG',
+    '#fffffG',
 ])
-def test_hex_to_rgv_error(hex_error_value):
+def test_hex_to_rgb_error(hex_error_value):
+    """
+    Test if the given hex values are invalid
+    """
     with pytest.raises(ValueError):
         utils.hex_to_rgb(hex_error_value)
