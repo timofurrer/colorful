4d72903ba1c8e4e98870a03b2e626870ca2640f4
fliiiix <hi@l33t.name>
2025-10-29 09:55:44 +0100

    Add Python 3.14 support

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.12',
             'Programming Language :: Python :: 3.13',
+            'Programming Language :: Python :: 3.14',
             'Programming Language :: Python :: Implementation',
             'Programming Language :: Python :: Implementation :: CPython',
             'Programming Language :: Python :: Implementation :: PyPy'
